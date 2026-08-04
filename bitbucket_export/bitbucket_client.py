a2f4fbd9faa76159ae50c199d20b26b717d456cb
Campbell Brown <campbell.brown@bduncanltd.com>
2026-08-05 11:33:56 +1200

    CI fix

--- bitbucket_export/bitbucket_client.py
+++ bitbucket_export/bitbucket_client.py
@@ -14,7 +14,6 @@ import base64
 import json
 import logging
 import time
-import typing
 from collections.abc import Iterator
 from pathlib import Path
 from urllib.error import HTTPError
@@ -41,8 +40,8 @@ class _StripAuthOnRedirect(HTTPRedirectHandler):
     S3 makes it reject the request, so we remove it whenever we cross hosts.
     """
 
-    @typing.override
-    def redirect_request(self, req, fp, code, msg, headers, newurl):
+    # The signature is dictated by HTTPRedirectHandler.
+    def redirect_request(self, req, fp, code, msg, headers, newurl):  # noqa: PLR0913, PLR0917
         new_request = super().redirect_request(req, fp, code, msg, headers, newurl)
         if new_request is None:
             return None
